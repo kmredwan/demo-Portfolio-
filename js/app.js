2ed25a1fe0eccb90d0102d8760c22d70773bdecf
K M Redwan <97582958+kmredwan@users.noreply.github.com>
2022-03-12 00:50:34 +0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $(".slider").slick({
         arrows:false ,
         dots:true,
+        autoplay:true,
         appendDots:'.slider_dots',
         dotsClass:'dots'
 
