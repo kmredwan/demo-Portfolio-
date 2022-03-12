14092ded091bbd560bfc9f9af6fd889a9af67356
K M Redwan <97582958+kmredwan@users.noreply.github.com>
2022-03-13 02:58:08 +0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -8,5 +8,20 @@ $(document).ready(function () {
 
 
     });
+
+    let hambarger= document.querySelector(".hamberger");
+    let times= document.querySelector(".times");
+    let mobileNab= document.querySelector(".mobile-nav");
+
+    hambarger.addEventListener('click',function(){
+        mobileNab.classList.add('open')
+
+
+    });
+
+    times.addEventListener('click',function(){
+        mobileNab.classList.remove('open')
+
+    });
 });
 
